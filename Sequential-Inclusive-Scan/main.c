a9ca0baacd36a02148e58e8147ff7e18661cca17
Ben3940 <bty2015@comcast.net>
2024-11-09 13:30:34 -0500

    Created test array and included stdio.h for printf

--- Sequential-Inclusive-Scan/main.c
+++ Sequential-Inclusive-Scan/main.c
@@ -1,7 +1,14 @@
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
 
-    printf("Hello, World!");
+    int arr[] = {1, 2, 3};
+
+    for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+    {
+        printf("element %i: %i\n", i, arr[i]);
+    }
+
     return 0;
 }
