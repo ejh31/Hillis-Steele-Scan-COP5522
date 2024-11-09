b7012107415d76695fed3a82c6eed730ab079ef0
Ben3940 <bty2015@comcast.net>
2024-11-09 14:04:23 -0500

    Implemented sequential inclusive scan algorithm

--- Sequential-Inclusive-Scan/main.c
+++ Sequential-Inclusive-Scan/main.c
@@ -1,14 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void init_array(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+        arr[i] = 0;
+}
+
+void print_array_elements(int *arr, int size)
+{
+    printf("[");
+    for (int i = 0; i < size; i++)
+        printf(" %i ", arr[i]);
+    printf("]");
+}
+
+void compute_inclusive_scan(int *input, int *output, int size)
+{
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += input[i];
+        output[i] = sum;
+    }
+}
 
 int main(int argc, char **argv)
 {
 
-    int arr[] = {1, 2, 3};
+    int input[] = {1, 2, 3};
+    int size = sizeof(input) / sizeof(int);
+    int *output = malloc(sizeof(input) * sizeof(int));
 
-    for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
-    {
-        printf("element %i: %i\n", i, arr[i]);
-    }
+    init_array(output, size);
+
+    compute_inclusive_scan(input, output, size);
+
+    printf("Input Array: \t");
+    print_array_elements(input, size);
+    printf("\n");
+    printf("Output Array: \t");
+    print_array_elements(output, size);
+
+    free(output);
 
     return 0;
 }
