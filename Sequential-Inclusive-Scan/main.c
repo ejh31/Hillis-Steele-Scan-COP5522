bc7643a25cba878814e9a0f9f262765ca6169eca
Ben Yanick <35115507+Ben3940@users.noreply.github.com>
2024-11-09 14:28:20 -0500

    Sequential Inclusive Scan (#1)
    * Created test array and included stdio.h for printf
    
    * Implemented sequential inclusive scan algorithm
    
    * Added array print functions and command line arg for input array size

--- Sequential-Inclusive-Scan/main.c
+++ Sequential-Inclusive-Scan/main.c
@@ -1,7 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void init_zero_array(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+        arr[i] = 0;
+}
+
+void init_input_array(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+        arr[i] = i + 1;
+}
+
+void print_array_elements(char *label, int *arr, int size)
+{
+    printf("%s \t [", label);
+    for (int i = 0; i < size; i++)
+        printf("%3i", arr[i]);
+    printf("  ]");
+}
+
+void compute_inclusive_scan(int *input, int *output, int size)
+{
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += input[i];
+        output[i] = sum;
+    }
+}
 
 int main(int argc, char **argv)
 {
+    if (argc != 2)
+    {
+        printf("ERROR\n");
+        printf("USAGE: ./main <number_input_elements>");
+        return 1;
+    }
 
-    printf("Hello, World!");
+    int size = atoi(argv[1]);
+    int *input = malloc(sizeof(int) * size);
+    int *output = malloc(sizeof(int) * size);
+
+    init_input_array(input, size);
+    init_zero_array(output, size);
+
+    compute_inclusive_scan(input, output, size);
+
+    print_array_elements("Input Array", input, size);
+    printf("\n");
+    print_array_elements("Output Array", output, size);
+
+    free(output);
+
     return 0;
 }
